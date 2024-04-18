4e6d6eac1729482a0cb226f92e47ee80d54f26a6
diogocorreiaf <diogocorreiaf@gmail.com>
2024-04-18 17:07:44 +0300

    Model loaded

---
+++ models/models.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+
+
+def FCN_VGG8(Img_Width,Img_Height,num_classes):
+
+  Input = tf.keras.layers.Input(shape = [Img_Width,Img_Height,3])
+  Conv1 = tf.keras.layers.Conv2D(64,kernel_size=3,strides = 1,padding="same",activation="relu")(Input)
+  Conv2 = tf.keras.layers.Conv2D(64,kernel_size=3,strides = 1,padding="same",activation="relu")(Conv1)
+  Pool1 = tf.keras.layers.MaxPool2D(pool_size=2,strides=2)(Conv2)
+
+  Conv3 = tf.keras.layers.Conv2D(128,kernel_size=3,strides = 1,padding="same",activation="relu")(Pool1)
+  Conv4 = tf.keras.layers.Conv2D(128,kernel_size=3,strides = 1,padding="same",activation="relu")(Conv3)
+  Pool2 = tf.keras.layers.MaxPool2D(pool_size=2,strides=2)(Conv4)
+
+  Conv5 = tf.keras.layers.Conv2D(256,kernel_size=3,strides = 1,padding="same",activation="relu")(Pool2)
+  Conv6 = tf.keras.layers.Conv2D(256,kernel_size=3,strides = 1,padding="same",activation="relu")(Conv5)
+  Conv7 = tf.keras.layers.Conv2D(256,kernel_size=3,strides = 1,padding="same",activation="relu")(Conv6)
+  Pool3 = tf.keras.layers.MaxPool2D(pool_size=2,strides=2)(Conv7)
+
+  Conv8 = tf.keras.layers.Conv2D(512,kernel_size=3,strides = 1,padding="same",activation="relu")(Pool3)
+  Conv9 = tf.keras.layers.Conv2D(512,kernel_size=3,strides = 1,padding="same",activation="relu")(Conv8)
+  Conv10 = tf.keras.layers.Conv2D(512,kernel_size=3,strides = 1,padding="same",activation="relu")(Conv9)
+  Pool4 = tf.keras.layers.MaxPool2D(pool_size=2,strides=2)(Conv10)
+
+  Conv11 = tf.keras.layers.Conv2D(512,kernel_size=3,strides = 1,padding="same",activation="relu")(Pool4)
+  Conv12 = tf.keras.layers.Conv2D(512,kernel_size=3,strides = 1,padding="same",activation="relu")(Conv11)
+  Conv13 = tf.keras.layers.Conv2D(512,kernel_size=3,strides = 1,padding="same",activation="relu")(Conv12)
+  Pool5 = tf.keras.layers.MaxPool2D(pool_size=2,strides=2)(Conv13)
+
+  # Fully Convolutional Layer
+
+  FC_Layer = tf.keras.layers.Conv2D(4096,kernel_size=7,activation="relu")(Pool5)
+  FC_Drop = tf.keras.layers.Dropout(rate=0.5)(FC_Layer)
+  FC_Layer2 = tf.keras.layers.Conv2D(4096,kernel_size=1,activation="relu")(FC_Drop)
+  FC_Drop2 = tf.keras.layers.Dropout(rate=0.5)(FC_Layer2)
+
+  # Classification Score Layer
+  Score = tf.keras.layers.Conv2D(num_classes,kernel_size=1,activation="relu")(FC_Drop2)
+ 
+
+  #Upsample Pool4
+
+  Upscore = tf.keras.layers.Conv2DTranspose(num_classes,kernel_size=4,strides=2,kernel_initializer="zeros")(Score)
+  
+  Conv_Scale = tf.keras.layers.Conv2D(num_classes,kernel_size=1)(Pool4)
+  Cropped = tf.keras.layers.Cropping2D(cropping=(5,5))(Conv_Scale)
+
+  Fused = tf.keras.layers.add([Cropped,Upscore])
+  
+  Upsampled_Pool4 = tf.keras.layers.Conv2DTranspose(num_classes,kernel_size=4,strides=2,kernel_initializer="zeros")(Fused)
+
+  
+  # Upsample Pool3
+
+  Conv_Scale2 = tf.keras.layers.Conv2D(num_classes,kernel_size=1)(Pool3)
+  Cropped2 = tf.keras.layers.Cropping2D(cropping=(9,9))(Conv_Scale2)
+  Fused2 = tf.keras.layers.add([Cropped2,Upsampled_Pool4])
+
+  Upsampled_Pool3 = tf.keras.layers.Conv2DTranspose(num_classes,kernel_size=128,strides=16,kernel_initializer="zeros")(Fused2)
+
+  # Score per Pixel
+
+  Score = tf.keras.layers.Cropping2D(cropping=(24,24))(Upsampled_Pool3)
+  Score = tf.keras.layers.Softmax(dtype = "float32")(Score)
+
+  return tf.keras.Model(inputs = Input,outputs = Score)
