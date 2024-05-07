17d915f585345f0024921d9885d353f078fc4aaf
diogocorreiaf <diogocorreiaf@gmail.com>
2024-05-07 23:04:07 +0300

    Testing against model

---
+++ test/testing.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+def segmentation_model_test():
+
+
+
+
+
+
+
+
+
+
+
+def detection_model_test
